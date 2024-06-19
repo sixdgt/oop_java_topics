8f42a4010e33b62c28a546217b13c8f816fb2a4b
sixdgt <c4crypt@gmail.com>
2024-06-19 09:38:56 +0545

    Unit One Example - TU BCA

---
+++ UnitOneExamples/src/unitone/datatype/DatatypeExample.java
@@ -0,0 +1,18 @@
+package unitone.datatype;
+
+public class DatatypeExample {
+    public static void main(String args[]){
+        // datatype example
+        int reg_no = 45;
+        String name = "DAV College";
+        double latitude = 87.457;
+        float longitude = 88.7f;
+        char a = 'a';
+        boolean college_status = true;
+        
+        // printing data in console
+        System.out.println("Registration No: " + reg_no);
+        System.out.print("Name: " + name);
+        System.out.print("latitude: " + latitude);
+    }
+}
